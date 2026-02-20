db9c1728445bc6b7da9816a7a097621deb06189c
erikordinegarcia <erik.garciaordine@hotmail.com>
2026-02-20 12:09:52 -0300

    mudanças

--- vite.config.ts
+++ vite.config.ts
@@ -5,17 +5,19 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/RenanPortoes/',
+
   server: {
-    host: "::",
+    host: '::',
     port: 8080,
     hmr: {
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react(), mode === 'development' && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 }));
